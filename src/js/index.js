325126b8e7e9f4c04f71c5d353c3de92d09599b2
Petervrancken <peter.vrancken88@gmail.com>
2021-03-12 01:01:06 +0100

    added codesandbox link

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,3 @@
 import "../../icons/svgxuse";
 
-import Card from "./Card.js";
 
-//new Card(document.getElementById("grid"), "icon-home")
-//new Card(document.getElementById("grid"), "icon-home")
-
-
-
-
-//window.addEventListener("flipped", function(e){
-    //console.log(e.detail._icon)
-//})
